1bc76c265847e73cf0ccea2f78cdc1a3ea4d0d29
Ricardo Casarez <Ricardo.CasarezGarcia@charter.com>
2022-03-09 10:21:35 -0800

    added documentation

--- app/src/main/java/com/example/kotlintask/FileUtil.kt
+++ app/src/main/java/com/example/kotlintask/FileUtil.kt
@@ -3,6 +3,12 @@ package com.example.kotlintask
 import android.content.Context
 
 object FileUtil {
+    /**
+     * Reads file from raw resources.
+     * @param context Android context to get access to resources.
+     * @param rawId Resource generated identifier.
+     * @return Returns a string with the file content.
+     */
     fun readRaw(context: Context, rawId: Int): String {
         return context.resources.openRawResource(rawId)
             .bufferedReader().use { bufferedReader -> bufferedReader.readText() }
