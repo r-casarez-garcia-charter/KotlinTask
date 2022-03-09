4e4701838027936cc124ff9a6a8232e18d27caf9
Ricardo Casarez <Ricardo.CasarezGarcia@charter.com>
2022-03-09 14:23:42 -0800

    updated readme

--- Readme.md
+++ Readme.md
@@ -1,18 +1,34 @@
-The project contains an empty activity and has dependencies to most common android libraries.
+The project contains an empty activity and has the most common android dependencies:
+* gson
+* coroutines
+* rxjava2, rxAndroid.
+* okhttp
+* retrofit (gson converter, kotlinx serialization converter and rxjava2 call adapter)
+    *  gson converter.
+    * kotlinx serialization converter
+    * rxjava2 adapter.
+* lifecycle
+    * viewmodel
+    * livedata
+    * runtime-ktx
+
+Feel free to use any of the available dependencies or add new ones.
 
 #### Task 1: Read the order data from res/raw/order.json
 Utility class FileUtility can be used to read the raw resource.
 ```kotlin
 FileUtil.readRaw(context: Context, rawId: Int): String
-```  
-#### Task 2: Get a list of unique types of food from the order in task1.
-#### Task 3: Calculate the price of pizza in the provided order using the following rules:
+```
+#### Task 2: Calculate the price of pizza in the provided order using the following rules:
 1. Size: small=5€, medium=10€, big=15€
 2. Toppings: 1€ each
 3. Sauce: 5€ for sauce regardless of color / count
 
-#### Task 4: Calculate the pizza price in USD
+#### Task 3: Calculate the pizza price in USD
 Fetch the exchange rate from the following location: https://gist.githubusercontent.com/darkalor/8b916a24ee746c432165ecefeeb5831a/raw/992f1ca2db87c83d58af8cb8d105dd88790a3195/euroPrice.json
+
+#### Show price.
+Show the total price in Euros and USD, prices can be displayed on the screen or logged to the console.
 
 ### Tips.
 Feel freer to use any json parser library of your preference. Dependency included for Gson and kotlin Serializatrion.
--- app/src/main/java/com/example/kotlintask/NetworkModule.kt
+++ app/src/main/java/com/example/kotlintask/NetworkModule.kt
@@ -6,11 +6,19 @@ import kotlinx.serialization.json.JsonConfiguration
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
+    /**
+     * Default OkHttpClient instance with logging.
+     */
+    fun provideOkHttpClient(): OkHttpClient = provideOkHttpClientBuilder()
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+        .build()
+
     fun provideOkHttpClientBuilder(): OkHttpClient.Builder {
         return OkHttpClient.Builder()
             .ignoreAllSSLErrors() // required for gist
@@ -19,15 +27,18 @@ object NetworkModule {
     fun provideRetrofitBuilderGson(): Retrofit.Builder =
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
 
     fun provideRetrofitBuilderSerialization(): Retrofit.Builder {
         return Retrofit.Builder()
-            .addConverterFactory(Json { ignoreUnknownKeys = true }.asConverterFactory(provideJsonConverterType()))
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(Json {
+                ignoreUnknownKeys = true
+            }.asConverterFactory(provideJsonConverterType()))
     }
 
-    fun provideJsonConverterType(): MediaType {
+    private fun provideJsonConverterType(): MediaType {
         return "application/json".toMediaType()
     }
+
+    private fun provideRxJavaFactory(): RxJava2CallAdapterFactory =
+        RxJava2CallAdapterFactory.create()
 }
